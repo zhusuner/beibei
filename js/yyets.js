9b5ef23a23dd0c1b3ea94601e95209234cbdef6e
zhusuner <80241679+zhusuner@users.noreply.github.com>
2023-09-02 23:04:28 +0800

    Update yyets.js

--- js/yyets.js
+++ js/yyets.js
@@ -14,7 +14,7 @@ var rule = {
 		'User-Agent': PC_UA,
 		'Accept': '*/*',
 		'Referer': 'https://yyets.click/',
-         	'Cookie':'http://127.0.0.1:9978/file:///tvbox/JS/lib/yyets.txt',
+         	'Cookie':'PPA_CI=ff8dba84f5bc281b486e025ca7abba69; cf_clearance=tC2D.k_.ettp3x2QtdkcvAE5UNSphF.m8fVMADGy7jU-1693666826-0-1-6704c06d.48c5173c.30059f8d-250.0.0; _ga=GA1.1.1767290962.1693666855; adblock=1; __gads=ID=af6fbc6bd0ada883-22d154fcbfe30047:T=1693666860:RT=1693666860:S=ALNI_MaOm0ALWvBZuTB9pOSyJESwHIGDTg; __gpi=UID=00000c39c16013c5:T=1693666860:RT=1693666860:S=ALNI_MbP1eGArRGxygGFD_ExuAt6Fqcfcg; _oauth_request_token="WklJNDFRQUFBQUFCby1FN0FBQUJpbFpwbjNF|ZjA1aDg4SHVXQXZ2WFNwUGtpT0xudWJteTRkWWpRWWw="; username="2|1:0|10:1693666903|8:username|12:emh1c3VuZXI=|ceef527c29fb9c534fd6ce9394524031797de938b97df67d491f3f138f54da0e"; _ga_K76PSKSSGX=GS1.1.1693666854.1.1.1693666990.52.0.0; FCNEC=%5B%5B%22AKsRol8pORq-NgHE080HAjke1QEkFbLuD4cbZqqN9oF0oa18s8zWHzhPMwny1VaxRFKKWUh_YN7tJbHBqjdhzVr6kyXQInxLHQyzIRtA9Bmb-bLg5zbhM6hcyCnOXpn2aCXeC_2ViPE461sS29keWI7fotXTu4Uogg%3D%3D%22%5D%2Cnull%2C%5B%5D%5D',
 	},
 	timeout:5000,
 	class_name:'',
